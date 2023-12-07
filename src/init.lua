6eb0babeffe56d099cbec0864cdc06831dad0ada
TheBetterDark@TheBetterDark <91607604+TheBetterDark@users.noreply.github.com>
2023-12-07 23:10:01 +0000

    Complete rewrite

--- src/init.lua
+++ src/init.lua
@@ -1,129 +1,129 @@
-type AnimationProperties = {
-	Name: string,
-	AnimationId: string,
-	AdjustSpeed: number?,
-	Weight: number?,
-	FadeIn: number?,
-	FadeOut: number?,
-	Looped: boolean?,
-	Duration: number?,
-}
-
-type AnimatonType = {
-	LoadAnimation: (name: string, animation: Animation) -> AnimationTrack,
-	UnloadAnimation: (name: string) -> nil,
+export type AnimatorClass = {
+	LoadAnimation: (animationName: string, animationId: Animation) -> AnimationTrack,
 	PlayAnimation: (animation: string, properties: AnimationProperties) -> AnimationTrack,
 	StopAnimation: (animation: string, properties: AnimationProperties) -> AnimationTrack,
 	StopAllAnimations: (properties: AnimationProperties) -> nil,
 }
 
+export type AnimationProperties = {
+	AdjustSpeed: number,
+	Weight: number,
+	FadeIn: number,
+	FadeOut: number,
+	Looped: boolean,
+	Priority: number,
+}
+
+export type PlayingAnimation = {
+	AnimationTrack: AnimationTrack,
+	Properties: AnimationProperties,
+}
+
+local Janitor = require(script.Parent.Janitor)
+
 local AnimaLib = {}
 AnimaLib.__index = AnimaLib
 
-function AnimaLib.new(humanoid: Humanoid): AnimatonType
-	local self = setmetatable({}, AnimaLib)
-	self.Humanoid = humanoid
-	self.Animator = humanoid:FindFirstChildOfClass("Animator") or humanoid
+function AnimaLib.new(animator: Animator): AnimatorClass
+	assert(typeof(animator) == "Instance" and animator:IsA("Animator"), "AnimaLib: Expected animator to be an 'Animator' type")
+
+	local self = {}
+	self.Animator = animator
 
 	self.LoadedAnimations = {}
 	self.PlayingAnimations = {}
 
-	return self
-end
-
-function createAnimationInstance(animationId)
-	local instance = Instance.new("Animation")
-	instance.AnimationId = animationId
-	return instance
+	return setmetatable(self, AnimaLib)
 end
 
 function AnimaLib:LoadAnimation(animationName: string, animationId: number): AnimationTrack
+	assert(type(animationName) == "string", "AnimLib: Expected animationName to be a string.")
+	assert(type(animationId) == "string", "AnimLib: Expected animationId to be a string.")
+
 	if self.LoadedAnimations[animationName] then
-		warn("AnimaLib: '" .. animationName .. "' is already loaded.")
-		return
+		error(string.format("AnimaLib: Unable to load animation. '%s' already exists.", animationName))
 	end
 
-	-- Check if animationId is in the correct format
-	if typeof(animationId) ~= "number" then
-		error("AnimaLib: '" .. tostring(animationId) .. "' is not a valid AnimationId.")
+	if animationId:sub(1, 13) ~= "rbxassetid://" then
+		error(string.format("AnimaLib: '%s' is not a valid AnimationId.", tostring(animationId)))
 	end
 
-	local animation = createAnimationInstance(`rbxassetid://${animationId}`)
+	if tonumber(animationId:sub(14, #animationId)) == nil then
+		error(string.format("AnimaLib: AnimationId contains invalid characters > '%s'", tostring(animationId)))
+	end
+
+	local animation = Instance.new("Animation")
+	animation.AnimationId = animationId
+
 	self.LoadedAnimations[animationName] = self.Animator:LoadAnimation(animation)
-
-	return self.LoadedAnimations[animationName.Name]
+	return self.LoadedAnimations[animationName]
 end
 
-function AnimaLib:UnloadAnimation(name: string)
-	if not self.LoadedAnimations[name] then
-		error("AnimaLib: Unable to unload animation '" .. name .. "' because it is not loaded.")
-	end
-	self.LoadedAnimations[name] = nil
+function AnimaLib:GetAnimationTrack(animationName: string): AnimationTrack
+	assert(type(animationName) == "string", "AnimLib: Expected animationName to be a string.")
+	assert(self.LoadedAnimations[animationName], string.format("AnimaLib: Unable to get animation '%s' because it is not loaded.", animationName))
+	return self.LoadedAnimations[animationName]
 end
 
-function AnimaLib:PlayAnimation(animationName: string, properties: AnimationProperties, ignorePlaying: boolean): AnimationTrack
-	assert(self.LoadedAnimations[animationName], "AnimaLib: Unable to play animation '" .. animationName .. "' because it is not loaded.")
+function AnimaLib:PlayAnimation(animationName: string, properties: AnimationProperties, ignorePlaying: boolean)
+	assert(self.LoadedAnimations[animationName], string.format("AnimaLib: Unable to play animation '%s' because it is not loaded.", animationName))
 
 	if self.PlayingAnimations[animationName] and not ignorePlaying then
-		warn("AnimaLib: '" .. animationName .. "' is already playing.")
+		warn(string.format("AnimaLib: '%s' is already playing.", animationName))
 		return
 	end
 
-	local animationTrack = self.LoadedAnimations[animationName]
-	properties.AdjustSpeed = properties and properties.AdjustSpeed or 1
+	local animationTrack: AnimationTrack = self.LoadedAnimations[animationName]
+	properties.Speed = properties and properties.AdjustSpeed or 1
 	properties.Weight = properties and properties.Weight or 1
 	properties.FadeIn = properties and properties.FadeIn or 0
 	properties.FadeOut = properties and properties.FadeOut or 0
 	properties.Looped = properties and properties.Looped or false
-	properties.Duration = properties and properties.Duration or false
 
-	animationTrack:Play(properties.FadeIn, properties.Weight, properties.AdjustSpeed)
 	animationTrack.Looped = properties.Looped
+	animationTrack:Play(properties.FadeIn, properties.Weight, properties.Speed)
 
 	self.PlayingAnimations[animationName] = {
 		AnimationTrack = animationTrack,
 		Properties = properties,
 	}
 
-	if properties.Duration then
-		task.delay(properties.Duration, function()
-			animationTrack:Stop(properties.FadeOut)
-			self.PlayingAnimations[animationName] = nil
-		end)
-	else
-		if not properties.Looped then
-			task.delay(animationTrack.Length, function()
-				animationTrack:Stop(properties.FadeOut)
-				self.PlayingAnimations[animationName] = nil
-			end)
-		end
-	end
+	local janitor = Janitor.new()
+	janitor:Add(animationTrack.Stopped:Connect(function()
+		janitor:Destroy()
+		self.PlayingAnimations[animationName] = nil
+	end))
 
 	return animationTrack
 end
 
-function AnimaLib:StopAnimation(animationName: string): nil
-	assert(self.PlayingAnimations[animationName], "AnimaLib: Unable to stop animation '" .. animationName .. "' because it is not playing.")
+function AnimaLib:StopAnimation(animationName: string, overrideFadeOut)
+	assert(self.LoadedAnimations[animationName], string.format("AnimaLib: Unable to stop animation '%s' because it is not loaded.", animationName))
 
-	local playingAnimation = self.PlayingAnimations[animationName]
-	local animationTrack: AnimationTrack = playingAnimation.AnimationTrack
-	local properties: AnimationProperties = playingAnimation.Properties
+	local playingAnimation: PlayingAnimation = self.PlayingAnimations[animationName]
+	if not playingAnimation then
+		return
+	end
+
+	local animationTrack = playingAnimation.AnimationTrack
+	local FadeOut = overrideFadeOut or playingAnimation.Properties.FadeOut
 
 	if animationTrack.IsPlaying then
-		animationTrack:Stop(properties.FadeOut)
+		animationTrack:Stop(FadeOut)
 	end
 end
 
-function AnimaLib:StopAllAnimations(): nil
-	for animationName, playingAnimation in pairs(self.LoadedAnimations) do
-		local AnimationTrack: AnimationTrack = playingAnimation.AnimationTrack
-		local properties: AnimationProperties = playingAnimation.Properties
-
-		if AnimationTrack.IsPlaying then
-			AnimationTrack:Stop(properties.FadeOut)
-			self.PlayingAnimations[animationName] = nil
-		end
+function AnimaLib:StopAllAnimations()
+	for animationName, _ in pairs(self.PlayingAnimations) do
+		self:StopAnimation(animationName)
 	end
+	task.wait()
+end
+
+function AnimaLib:Destroy()
+	self:StopAllAnimations()
+	self.LoadedAnimations = nil
+	self.PlayingAnimations = nil
 end
 
 return AnimaLib
